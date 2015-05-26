65eadf9c8689a26ddd48321144fddf502ac102a7
dl-eric <orangepamelo@gmail.com>
2015-05-26 12:43:34 -0400

    MainMenu

---
+++ core/src/com/enix/balancegame1/screens/MainMenu.java
@@ -0,0 +1,51 @@
+package com.enix.balancegame1.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Eric on 5/26/2015.
+ */
+public class MainMenu implements Screen {
+
+    @Override
+    public void render(float delta)
+    {
+
+    }
+
+    @Override
+    public void resize(int width, int height)
+    {
+
+    }
+
+    @Override
+    public void show()
+    {
+
+    }
+
+    @Override
+    public void pause()
+    {
+
+    }
+
+    @Override
+    public void dispose()
+    {
+
+    }
+
+    @Override
+    public void resume()
+    {
+
+    }
+
+    @Override
+    public void hide()
+    {
+
+    }
+}
