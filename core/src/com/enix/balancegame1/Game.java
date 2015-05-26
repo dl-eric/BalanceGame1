cc51e93cc3082e1628d53c0db603ca85df240884
dl-eric <orangepamelo@gmail.com>
2015-05-26 14:48:04 -0400

    asd

--- core/src/com/enix/balancegame1/Game.java
+++ core/src/com/enix/balancegame1/Game.java
@@ -1,6 +1,8 @@
 package com.enix.balancegame1;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
@@ -19,7 +21,10 @@ public class Game implements Screen {
     @Override
     public void render(float delta)
     {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        debugRenderer.render(world, camera.combined);
     }
 
     @Override
@@ -34,12 +39,13 @@ public class Game implements Screen {
         world = new World(new Vector2(0, -9.81f), false);
         debugRenderer = new Box2DDebugRenderer();
 
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
     public void hide()
     {
-
+        dispose();
     }
 
     @Override
@@ -57,6 +63,8 @@ public class Game implements Screen {
     @Override
     public void dispose()
     {
+        world.dispose();
+        debugRenderer.dispose();
 
     }
 }
