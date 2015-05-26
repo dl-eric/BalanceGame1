e630b029b25cb06426f35e6d5aa13fa649d1b457
Sabine Nix <sabine.a.nix@gmail.com>
2015-05-26 12:21:42 -0400

    Test

--- core/src/com/enix/balancegame1/BalanceGame.java
+++ core/src/com/enix/balancegame1/BalanceGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+//test
 public class BalanceGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
