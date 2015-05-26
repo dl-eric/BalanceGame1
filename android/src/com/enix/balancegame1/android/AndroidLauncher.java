f19e086f08d58d1a416773e55dd523ce1cf7d3d6
dl-eric <orangepamelo@gmail.com>
2015-05-26 01:05:49 -0400

    Initial commit

---
+++ android/src/com/enix/balancegame1/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.enix.balancegame1.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.enix.balancegame1.BalanceGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BalanceGame(), config);
+	}
+}
