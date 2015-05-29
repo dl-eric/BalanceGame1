65b93b8fb18710c5fbee31cf05bbbafcfc8e2115
Sabine Nix <sabine.a.nix@gmail.com>
2015-05-29 12:48:46 -0400

    plswork

--- core/src/com/enix/balancegame1/BalanceGame.java
+++ core/src/com/enix/balancegame1/BalanceGame.java
@@ -1,20 +1,19 @@
 package com.enix.balancegame1;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.enix.balancegame1.screens.Splash;
 
-public class BalanceGame extends ApplicationAdapter {
+public class BalanceGame extends com.badlogic.gdx.Game {
 	SpriteBatch batch;
 	Texture img;
 
 	@Override
 	public void create ()
 	{
-//		setScreen(new Splash);
+		setScreen(new Splash());
 	}
 
 	@Override
