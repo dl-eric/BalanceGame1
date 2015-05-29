dfd6f0ec2d294de973eeba3b09f531b3d0a02b6e
dl-eric <orangepamelo@gmail.com>
2015-05-29 16:20:35 -0400

    asd

--- core/src/com/enix/balancegame1/screens/Splash.java
+++ core/src/com/enix/balancegame1/screens/Splash.java
@@ -45,7 +45,7 @@ public class Splash implements Screen
         tweenManager = new TweenManager();
         Tween.registerAccessor(Sprite.class, new SpriteAccessor());
 
-        Texture splashTexture = new Texture(Gdx.files.internal("img/splashscreen.jpeg"));
+        Texture splashTexture = new Texture(Gdx.files.internal("img/splashscreen.jpg"));
         splash = new Sprite(splashTexture);
         splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
