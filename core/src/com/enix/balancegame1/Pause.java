f823afdb49e4598af9da31fc77847bbaac82b77f
dl-eric <orangepamelo@gmail.com>
2015-06-07 23:25:17 -0400

    lkkol

--- core/src/com/enix/balancegame1/Pause.java
+++ core/src/com/enix/balancegame1/Pause.java
@@ -1,7 +1,44 @@
 package com.enix.balancegame1;
 
+import com.badlogic.gdx.Screen;
+
 /**
  * Created by Eric on 6/7/2015.
  */
-public class Pause {
+public class Pause implements Screen
+{
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
